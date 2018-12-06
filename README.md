c31a361ce79511e9c6f12c7460646eb893a46069
hubenchang0515 <hubenchang0515@outlook.com>
2018-12-06 21:57:37 +0800

    some little change

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Markdown语法
 使用 GitHub API ( [https://developer.github.com/v3/markdown/](https://developer.github.com/v3/markdown/) ) 来渲染Markdown，因此支持的语法与GitHub一致。
-* 请确保搭建博客所用的服务器能够正常请求GitHub API
+* 请确保搭建博客所用的服务器能够正常请求GitHub API，请求GitHub API的速度会影响网站的加载速度。
 
 ## Python与依赖库
 `Phosphophyllite`采用`Python3`，依赖以下库:
@@ -13,10 +13,10 @@ import requests
 import sqlite3
 ```
 
-## Feature    
-* 文章中如需使用`<`和`>`，必须使用对应的转义字符`&lt;`和`&gt;`  
-* 由于GitHub API的限制，文章的大小不能超过`400KB`
-<span class="color:red;">红色</span> 
+## 注意事项    
+* 文章中如需使用`<`和`>`，必须使用对应的转义字符`&lt;`和`&gt;`。  
+* 由于GitHub API的限制，文章的大小不能超过`400KB`(约10万字)。  
+* 出于安全性考虑，GitHub API会过滤掉Markdown中html标签的属性，因此无法使用类似`<span style="color:red;">TEXT</span>`的方式自定义文本样式。
 
 ## TODO List
  - [x] Markdown渲染
--- module/MarkdownView.py
+++ module/MarkdownView.py
@@ -2,7 +2,10 @@ import requests
 import json
 
 def renderMarkdown(md_text) :
-    payload = {"text" : md_text, "mode" : "gfm", "context" : "https://github.com/hubenchang0515/Phosphophyllite"}
-    response = requests.post("https://api.github.com/markdown", data=json.dumps(payload))
-    output = response.text #.encode("utf-8").decode("utf-8")
-    return output
+    try :
+        payload = {"text" : md_text, "mode" : "gfm", "context" : "https://github.com/hubenchang0515/Phosphophyllite"}
+        response = requests.post("https://api.github.com/markdown", data=json.dumps(payload))
+        output = response.text #.encode("utf-8").decode("utf-8")
+        return output
+    except Exception as e:
+        return "<h1>Exception</h1>\n<p>%s</p>\n<p>%s</p>" % (e, md_text)
