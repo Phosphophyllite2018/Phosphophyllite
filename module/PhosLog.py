f17e6162a3eaa978a63d57bc6dba61222d538df3
hubenchang0515 <hubenchang0515@outlook.com>
2018-12-07 22:17:47 +0800

    log to txt

--- module/PhosLog.py
+++ module/PhosLog.py
@@ -2,7 +2,7 @@
 import time
 
 def log(message) :
-    fp = open('./private/phosphophyllite.log', 'a',encoding='utf-8')
+    fp = open('./private/phosphophyllite.log.txt', 'a',encoding='utf-8')
     t = time.strftime("%Y-%m-%d %H:%M:%S")
     fp.write("[Log](%s) : %s \n" , (t, message))
     fp.close()
