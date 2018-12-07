23761fd5b2c736e344ee9d9ab0d014a69ae0f7bd
hubenchang0515 <hubenchang0515@outlook.com>
2018-12-07 22:50:41 +0800

    update PhosLog.py

--- module/Model/PhosLog.py
+++ module/Model/PhosLog.py
@@ -4,5 +4,7 @@ import time
 def log(message) :
     fp = open('./private/phosphophyllite.log.txt', 'a',encoding='utf-8')
     t = time.strftime("%Y-%m-%d %H:%M:%S")
-    fp.write("[Log](%s) : %s \n" , (t, message))
+    text = "[Log](%s) : %s \n" % (t, message)
+    print(text, end="")
+    fp.write(text)
     fp.close()
