fcd350fd68cb0fed75fdf791dd70ecea162b2df7
hubenchang0515 <hubenchang0515@outlook.com>
2018-12-08 20:53:09 +0800

    adjust color

---
+++ module/Model/BlogModel.py
@@ -0,0 +1,54 @@
+import sqlite3
+import hashlib
+import time
+from . import PhosLog
+
+def cursor() :
+    db = sqlite3.connect("./private/phosphophyllite.db")
+    cursor = db.cursor()
+    return cursor
+
+def getUsername() :
+    try :
+        sql = "SELECT username FROM blog WHERE id = 0;"
+        result = cursor().execute(sql)
+        return result.fetchone()[0]
+    except Exception as e:
+        PhosLog.log(e)
+        return "Phosphophyllite"
+
+def checkPassword(pswd) :
+    try :
+        sha256 = hashlib.sha256()
+        sha256.update(pswd.encode('utf-8'))
+        pswd = sha256.hexdigest()
+        sql = "SELECT password FROM blog WHERE id = 0;"
+        result = cursor().execute(sql)
+        if pswd == result.fetchone()[0] :
+            return True
+        else :
+            return False
+    except Exception as e:
+        PhosLog.log(e)
+        return False
+
+def getRunDays() :
+    try :
+        sql = "SELECT birthday FROM blog WHERE id = 0;"
+        result = cursor().execute(sql)
+        birthday = time.mktime(time.strptime(result.fetchone()[0], "%Y-%m-%d %H:%M:%S")) 
+        now = time.time()
+        return int((now - birthday)/(24*60*60))
+    except Exception as e:
+        PhosLog.log(e)
+        return 0
+
+def getVisiting() :
+    try :
+        sql = "SELECT visiting FROM blog WHERE id = 0;"
+        result = cursor().execute(sql)
+        return result.fetchone()[0]
+    except Exception as e:
+        PhosLog.log(e)
+        return 0
+
