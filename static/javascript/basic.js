7acf5927bf84ae270e2d965af2034e370b2b8cd8
hubenchang0515 <hubenchang0515@outlook.com>
2018-12-10 16:06:44 +0800

    js post and get

--- static/javascript/basic.js
+++ static/javascript/basic.js
@@ -1,6 +1,43 @@
+/* img标签源文件加载失败时绘制一张图片 */
 function imageLoadError(img)
 {
     img.onerror = null;
     let canvas = document.createElement("canvas");
-    img.src = canvas.toDataURL("canvas")
+    img.src = canvas.toDataURL("canvas");
+}
+
+
+/* 发起post请求 */
+function httpPost(url, params)
+{
+    let virtualForm = document.createElement("form");
+    virtualForm.action = url;
+    virtualForm.method = "post";
+    for(let key in params)
+    {
+        let input = document.createElement("textarea");
+        input.name = key;
+        input.value = params[key];
+        virtualForm.appendChild(input);
+    }
+    document.body.appendChild(virtualForm);
+    virtualForm.submit();
+}
+
+
+/* 发起get请求 */
+function httpPost(url, params)
+{
+    let virtualForm = document.createElement("form");
+    virtualForm.action = url;
+    virtualForm.method = "get";
+    for(let key in params)
+    {
+        let input = document.createElement("textarea");
+        input.name = key;
+        input.value = params[key];
+        virtualForm.appendChild(input);
+    }
+    document.body.appendChild(virtualForm);
+    virtualForm.submit();
 }
