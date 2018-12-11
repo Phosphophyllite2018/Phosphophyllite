017a76d1d31c94302083aaee06394d8914911406
hubenchang0515 <hubenchang0515@outlook.com>
2018-12-11 19:46:27 +0800

    fix a little bug

--- module/DemoPage.py
+++ module/DemoPage.py
@@ -15,6 +15,10 @@ def renderPage() :
     article_html = ArticleView.render("示例文章", "2018-12-07", "520", 
                                     MarkdownView.renderMarkdown(demo_article, gitname, gitpass))
 
+    # 最近文章
+    recent_article = ArticleModel.getRecentArticle(10) 
+    recent_article_html = ArticleView.renderAsideArticle(recent_article, gitname, gitpass)
+
     # 最近留言
     recent_message = MessageModel.getRecentMessage(10)
     recent_message_html = MessageView.renderAsideMessage(recent_message, gitname, gitpass)
@@ -25,7 +29,7 @@ def renderPage() :
                             visiting_count=AsideModel.getVisiting(), 
                             artcile_count=AsideModel.getArticles(), 
                             message_count=AsideModel.getMessages(),
-                            recent_article=demo_article,
+                            recent_article=recent_article_html,
                             recent_message=recent_message_html)
 
     return render_template('template.html', 
